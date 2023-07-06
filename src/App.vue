53a4e8604d5717af10faa6808efb677e9eb5c8e0
NaifKAbdullah <nabdullah2@wisc.edu>
2023-07-06 12:05:53 -0400

    Used slowed version

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@
     -->
 
       <video class="img__background" autoplay muted loop>
-        <source src="./assets/city.mp4" type="video/mp4">
+        <source src="./assets/Smucker-Headquarters-(Slowed-to-0.8).mp4" type="video/mp4">
         Your browser does not support HTML5 video.
       </video>
 
