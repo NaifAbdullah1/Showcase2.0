b5e3796cf0c3394026918bd4d2b8b00ba32312e5
NaifKAbdullah <nabdullah2@wisc.edu>
2023-07-06 14:46:13 -0400

    Added logos

--- src/App.vue
+++ src/App.vue
@@ -35,7 +35,8 @@
     -->
 
       <div class="intro-box">
-        <h1>Nathan Abdullah</h1>
+        <img src=""/>
+        <h1>Nathan's Showcase</h1>
         <div>
           <button class="button" @click="scrollToGetToKnowMeSection">Get to Know Me</button>
           <button class="button">See What I Have Accomplished</button>
@@ -56,51 +57,63 @@
   <div class="spacing"></div>
 
   <section class="horizontal__content" id="getToKnowMe">
-    <scroll-parallax :speed="0.65" :left="true" direction="x">
+    <scroll-parallax :speed="0.30" :left="true" direction="x">
       <div style="display: flex; justify-content: flex-start;">
         <img
-          class="img__background"
-          src="./assets/Smucker.jpg"
+          class="img__background horizontal__img"
+          src="./assets/OriPic(cropped).jpg"
         />
         <img
-          class="img__background"
-          src="./assets/OriPic.jpg"
+          class="img__background horizontal__img"
+          src="./assets/Smucker.jpg"
         />
       </div>
     </scroll-parallax>
 
+    <section class="horizontal__content" id="getToKnowMe">
+      <scroll-parallax :speed="0.15" :left="false" direction="x">
+        <div style="display: flex; justify-content: flex-end;">
+          <img
+            class="img__background horizontal__img"
+            src="./assets/GrpPic.jpg"
+          />
+          <img
+            class="img__background horizontal__img"
+            src="./assets/Orientation.jpg"
+          />
+        </div>
+      </scroll-parallax>
+    </section>
+
+    <!--
     <div class="horizontal__elements">
       <scroll-parallax
         :speed="0.15"
         direction="x"
       >
+      
       <div style="display: flex; align-item: center;">
-        
-        <!--
         <div class="img__title">
           <img src="https://images.unsplash.com/photo-1545062990-4a95e8e4b96d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1500&q=80" alt="">
         </div>
-        -->
-
-        <!--
         <div class="img__title" style="">
           <code>
               {{ scrollX }}
           </code>
         </div>
-        -->
-
-
       </div>
+      
       </scroll-parallax>
     </div>
+    -->
 
   </section>
 </template>
 
 <script>
 import ScrollParallax from './components/ScrollParallax.vue';
-import { onMounted, ref } from 'vue'
+import { onBeforeUnmount, onMounted } from 'vue'
+import { gsap } from "gsap"
 
 export default {
 
@@ -108,22 +121,47 @@ export default {
     ScrollParallax
   },
   setup(){
-    const scrollY = ref("")
-    const scrollX = ref("")
 
     onMounted(() => {
-      scrollY.value = '<scroll-parallax direction="y"></scroll-parallax>'
-      scrollX.value = '<scroll-parallax :left="true" direction="x"></scroll-parallax>'
+
+      document.title = "Nathan's Showcase"
+
+      // This makes the intro box fade in on page load
+      gsap.from ('.intro-box', { 
+        duration: 1, // Duration of fade in
+        opacity:0,  // Start from opacity of 0
+        delay:0.8 // Wait 0.8 seconds after the page loads before starting to fade in
+      })      
+
+      const introBox = document.querySelector('.intro-box')
+
+      // Slightly expands the intro box on mouse hover. 
+      introBox.addEventListener('mouseover', () => {
+        gsap.to('.intro-box', {
+          duration: 0.3, 
+          scale: 1.1 // Multiply the dimensions of the intro-box by 1.1, increase by 10%
+        })
+      })
+
+      // Revert the intro box to its original size after the mouse is off the intro box.
+      introBox.addEventListener('mouseout', () => {
+        gsap.to('.intro-box', { duration: 0.3, scale: 1 });
+      });
+
     })
 
+    // A JS function that runs when you click the "Get to Know Me" button that scrolls you down to the right section. 
     const scrollToGetToKnowMeSection = () => {
       const getToKnowMeSection = document.getElementById("getToKnowMe");
       getToKnowMeSection.scrollIntoView({behavior: 'smooth'})
     }
 
+
+    onBeforeUnmount(() => {
+
+    })
+
     return {
-      scrollY,
-      scrollX,
       scrollToGetToKnowMeSection 
     }
   },
@@ -135,12 +173,18 @@ export default {
     margin: 0;
     padding: 0;
     font-family: Helvetica;
+    height: 3000px;
   }
 
   .img__background {
     width: 100%;
     top: 0;
   }
+
+  .horizontal__img {
+    height: 937px; /* Set the height as needed */
+  }
+
   .img__title {
     position: relative;
     bottom: 30em;
@@ -157,17 +201,6 @@ export default {
 
   .spacing {
     height: 0px;
-  }
-
-  code {
-    color: orange;
-    background-color: rgba(0,0,0,1);
-    padding: 5px 15px;
-    border-radius: 4px;
-    box-shadow: 1px 2px 4px rgba(0,0,0,0.8);
-    text-shadow: none;
-    margin-left: 30px;
-    font-size: 16px;
   }
 
   .horizontal__content {
@@ -208,7 +241,9 @@ export default {
     box-shadow: 0 0 15px rgba(0,0,0,0.2);
     width: 600px;
     left: 35%;
+    /*
     transition: all 0.3s ease-in-out;
+    animation: fadeIn 1s ease-in 1s; */
   }
 
   .intro-box:hover {
@@ -220,6 +255,13 @@ export default {
     font-size: 42px;
     font-family: 'Petrona';
 }
+
+/*This will make the intro-box appear on page load 
+@keyframes fadeIn {
+  from {opacity: 0;}
+  to {opacity: 1;}
+}
+*/
 
 </style>
 
