1a758655a36292d2a4b96bb5c9303a7214201807
NaifKAbdullah <nabdullah2@wisc.edu>
2023-07-06 09:35:21 -0400

    Added pics

--- src/App.vue
+++ src/App.vue
@@ -60,7 +60,7 @@
       <div style="display: flex; justify-content: flex-start;">
         <img
           class="img__background"
-          src="https://images.unsplash.com/photo-1590880449155-b54f958ce314?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1430&q=80"
+          src="./assets/GrpPic.JPG"
         />
         <img
           class="img__background"
