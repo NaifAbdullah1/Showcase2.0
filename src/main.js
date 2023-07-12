47e6ea74ca8995980d84bde5dd0a7cb34946bcf7
NaifKAbdullah <nabdullah2@wisc.edu>
2023-07-12 09:25:24 -0400

    removed unnecessary scrolling library

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import "./assets/styles.css"
-import VueSmoothScroll from 'vue3-smooth-scroll'
+//import VueSmoothScroll from 'vue3-smooth-scroll' // Smooth scrolling library that enables you to controll scrolling speed and duration. 
 
 
 const app = createApp(App)
-app.use(VueSmoothScroll)
+//app.use(VueSmoothScroll)
 app.mount('#app')
